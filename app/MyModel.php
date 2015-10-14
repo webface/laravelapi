6aca2616b4ebd2df98d0343f34bd6b1753df9a30
Tommy Adeniyi <tommyadeniyi@gmail.com>
2015-10-14 18:09:33 -0400

    initial commit

---
+++ app/MyModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent;
+
+class MyModel extends Model{
+    
+    protected $fillable=['name','phone','secretAttribute','password'];
+    
+    protected $hidden = ['secretAttribute' , 'password'];
+}
+
