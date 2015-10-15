9416a80dcb6a5bee911ee6e5b82c53749d4d06ed
Tommy Adeniyi <tommyadeniyi@gmail.com>
2015-10-15 17:58:26 -0400

    Tweaked controllers

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -16,17 +16,10 @@ class MakerController extends Controller
     public function index()
     {
         //
+        //
+        return 'Im in index';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -50,16 +43,6 @@ class MakerController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
--- app/Http/Controllers/MakerVehiclesController.php
+++ app/Http/Controllers/MakerVehiclesController.php
@@ -13,20 +13,13 @@ class MakerVehiclesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
         //
+        //
+        return 'Showing the vehicles of '.$id;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -50,16 +43,6 @@ class MakerVehiclesController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
